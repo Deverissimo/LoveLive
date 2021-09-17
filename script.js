eb220fc4095c36e709a3793ad05b86a756eabb05
Matheus Deveríssimo <90214037+Deverissimo@users.noreply.github.com>
2021-09-17 12:27:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function newLine() {
+  document.write("<br>");
+}
+function docWrite(phrase) {
+  newLine();
+  document.write(phrase);
+}
+docWrite("<p> School Idol Project </p>");
+var µs = [
+  "https://i.idol.st/u/card/icon/518UR-Kousaka-Honoka-I-Don-t-Wanna-Get-My-Clothes-Wet-Tropical-Vacation-3nrDYy.png",
+  " https://i.idol.st/u/card/icon/459UR-Sonoda-Umi-Homura-Bakery-s-red-bean-paste-is-beyond-compare-Dreamy-Kids-Cz3Gou.png",
+  " https://i.idol.st/u/card/icon/520UR-Minami-Kotori-Gobble-Munch-Tropical-Squash-0dVS1J.png",
+  " https://i.idol.st/u/card/icon/539UR-Ayase-Eli-Ah-So-You-Have-Realized-What-I-Am-Venus-on-a-Moonlit-Night-Cjn6Xw.png",
+  " https://i.idol.st/u/card/icon/496UR-Hoshizora-Rin-A-Perfect-Day-for-Tanabata-Princess-of-the-Night-Sky-RnRej4.png",
+  " https://i.idol.st/u/card/icon/511UR-Nishikino-Maki-I-Especially-Like-This-Study-A-Captivating-Librarian-yn7HqE.png",
+  " https://i.idol.st/u/card/icon/504UR-Koizumi-Hanayo-It-s-so-fluffy-that-it-melts-in-my-mouth-TWINKLE-STARRY--WeXwfr.png",
+  " https://i.idol.st/u/card/icon/547UR-Yazawa-Nico-Hey-It-s-That-Legendary-Movie-Weather-Wizard-nReqrP.png",
+  " https://i.idol.st/u/card/icon/523UR-Tojo-Nozomi-It-s-Only-Just-Getting-Started-Summer-on-Stage-hf3eAS.png"
+];
+for (var i = 0; i < µs.length; i++) {
+  document.write("<img src=" + µs[i] + ">");
+}
+newLine();
+newLine();
+docWrite("<p> Love Live! Sunshine!! </p>");
+
+var aqours = [
+  " https://i.idol.st/u/card/icon/267UR-Takami-Chika-I-wanna-see-the-ocean-with-you-Chika-kyun-Summer-YcrqKr.png",
+  " https://i.idol.st/u/card/icon/499UR-Sakurauchi-Riko-Mmm-it-s-delicious-Princess-of-the-Night-Sky-Vt5YC2.png",
+  " https://i.idol.st/u/card/icon/525UR-Watanabe-You-Yaaay-Summer-on-Stage-4SpeKK.png",
+  " https://i.idol.st/u/card/icon/549UR-Kunikida-Hanamaru-This-Is-Just-like-Paradise-Zura-Weather-Wizard-PXkbk8.png",
+  " https://i.idol.st/u/card/icon/506UR-Tsushima-Yoshiko-Wherever-Yohane-goes-justice-shall-follow-TWINKLE-STAR-0nvqqw.png",
+  " https://i.idol.st/u/card/icon/512UR-Kurosawa-Ruby-Mm-Mumble-Mumble-Sweet-Little-Angel-pTgldk.png",
+  " https://i.idol.st/u/card/icon/442UR-Kurosawa-Dia-Aren-t-they-beautiful-Cherry-blossom-fairy-80gKo6.png",
+  " https://i.idol.st/u/card/icon/541UR-Ohara-Mari-Please-Grant-My-Heart-s-Desire-Venus-on-a-Moonlit-Night-kM6Fh6.png",
+  " https://i.idol.st/u/card/icon/517UR-Matsuura-Kanan-Sweets-Bigger-Than-They-Looked-Tropical-Vacation-ls00if.png"
+];
+for (var i = 0; i < aqours.length; i++) {
+  document.write("<img src=" + aqours[i] + ">");
+}
+newLine();
+newLine();
+docWrite("<p> Nijigasaki Gakuen </p>");
+
+var niji = [
+  " https://i.idol.st/u/card/icon/526UR-Uehara-Ayumu-That-Was-Such-a-Great-Show-Summer-on-Stage-7DeBIj.png",
+  " https://i.idol.st/u/card/icon/500UR-Nakasu-Kasumi-Kasumi-s-Request-Princess-of-the-Night-Sky-y7ddtV.png",
+  " https://i.idol.st/u/card/icon/542UR-Miyashita-Ai-How-Do-You-like-My-Illumination-Magic-Venus-on-a-Moonlit-N-6Naot4.png",
+  " https://i.idol.st/u/card/icon/503UR-Yuki-Setsuna-Here-s-one-for-you-Jet-Black-Wizard-SciHnN.png",
+  " https://i.idol.st/u/card/icon/531UR-Osaka-Shizuku-A-Refreshing-Foresty-Scent-Fluffy-Wuffy-Shizu-Bear-ZlbdV4.png",
+  " https://i.idol.st/u/card/icon/551UR-Tennoji-Rina-It-Doubles-as-a-Flashlight-Weather-Wizard-O5haQi.png",
+  " https://i.idol.st/u/card/icon/485UR-Mifune-Shioriko-It-s-delicious-Harmonic-Song-2eujGB.png",
+  " https://i.idol.st/u/card/icon/508UR-Asaka-Karin-This-tasted-spicier-than-I-expected-TWINKLE-STARRY-NIGHT-Lb1Xll.png",
+  " https://i.idol.st/u/card/icon/545UR-Emma-Verde-I-Feel-like-I-Could-Walk-Forever-Delivering-Comfort-Emma-Mai-iTMxsD.png",
+  " https://i.idol.st/u/card/icon/514UR-Konoe-Kanata-Fire-Mumble-Tropical-Vacation-Gw5nst.png",
+  " https://i.idol.st/u/card/icon/560SR-Lanzhu-Zhong-Come-On-Let-s-Go-Queendom-P1OQWF.png",
+  " https://i.idol.st/u/card/icon/563SR-Mia-Taylor-How-I-Do-Things-I-m-Still-VuctZw.png"
+];
+for (var i = 0; i < niji.length; i++) {
+  document.write("<img src=" + niji[i] + ">");
+}
+newLine();
+newLine();
+docWrite("<p> Love Live! Superstar!! </p>");
+
+var liella = [
+  "https://i.imgur.com/CYuCLlq.png",
+  "https://i.imgur.com/f3FzgoL.png",
+  "https://i.imgur.com/w1flsc6.png",
+  "https://i.imgur.com/nWwJnZ3.png",
+  "https://i.imgur.com/jjcw4oh.png"
+];
+
+for (var i = 0; i < liella.length; i++) {
+  document.write("<img class=" + "liellaImg" + " src=" + liella[i] + ">");
+}
